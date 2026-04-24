06bf5e333614045fc659fd119d48ffdd201e2c95
Agho Gracious <ivieagho29@gmail.com>
2026-04-24 18:53:05 +0100

    Add Render URL to ALLOWED_HOSTS

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['django-blog-gracey.onrender.com', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['django-blog-52oj.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
