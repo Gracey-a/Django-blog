295ca93f58cfb7d4af43cbfaa3f879054cafd5ae
Agho Gracious <ivieagho29@gmail.com>
2026-04-25 11:00:50 +0100

    Fix Cloudinary env var names

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -162,8 +162,8 @@ import cloudinary.uploader
 import cloudinary.api
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('dn5opu7w'),
-    'API_KEY': os.environ.get('568573767446757'),
-    'API_SECRET': os.environ.get('uB7Zeyx0Kq-kXEe-lJbqysytVlY'),
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
